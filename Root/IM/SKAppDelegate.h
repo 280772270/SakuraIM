d0b0bb1acebf058d428d0012e56c71ac83a1d68a
Z_yif <280772270@qq.com>
2017-02-16 15:21:20 +0800

    add images

---
+++ Root/IM/SKAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SKAppDelegate.h
+//  IM
+//
+//  Created by Tec_yifei on 17/2/15.
+//  Copyright © 2017年 Tec_yifei. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface SKAppDelegate : AppDelegate
+
+@end
